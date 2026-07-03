5eca1186c81c03bebdf9a243fab7127ec0cf0781
Sudhanshu Prasad <146873937+sudhanshu00011@users.noreply.github.com>
2026-07-03 18:54:25 +0530

    Implement binary search for search insert position

---
+++ Search Insert Position/search-insert-position.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left=0, right=nums.size()-1;
+        while (left<=right) {
+            int mid = left + (right - left) / 2; //to concise the TC
+            if (nums[mid] == target) return mid;
+            else if (target> nums[mid]) left=mid+1;
+            else right=mid-1;
+        }
+        return left;
+    }
+};

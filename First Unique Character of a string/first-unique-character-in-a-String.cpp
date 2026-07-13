22227580aae8830cee6d8f714e5c1e379b91596d
Sudhanshu Prasad <146873937+sudhanshu00011@users.noreply.github.com>
2026-07-13 10:49:40 +0530

    Implement first unique character finder in C++

---
+++ First Unique Character of a string/first-unique-character-in-a-String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int arr[26]= {0};
+        for (char c : s) {
+            arr[c-'a']++;
+        }
+        for (int i=0; i<s.length(); i++) {
+            if (arr[s[i]-'a'] == 1) return i;
+        }
+        return -1;
+    }
+};

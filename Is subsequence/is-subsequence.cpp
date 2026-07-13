adab30362cbe520399b368d50a0cbaa9a488313f
Sudhanshu Prasad <146873937+sudhanshu00011@users.noreply.github.com>
2026-07-13 20:11:56 +0530

    Implement isSubsequence function in C++

---
+++ Is subsequence/is-subsequence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        for (int j = 0; j < t.length() && i < s.length(); j++) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+        }
+        return i == s.length();
+    }
+};

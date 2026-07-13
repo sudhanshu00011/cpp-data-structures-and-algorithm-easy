83328fc35679c227a9f6d24a6c8c09bbd64a1c9d
Sudhanshu Prasad <146873937+sudhanshu00011@users.noreply.github.com>
2026-07-13 10:14:28 +0530

    Add guessNumber function for number guessing game
    Implement binary search to guess the number.

---
+++ Guess number higher or lower/guess-higher-lower.cpp
@@ -0,0 +1,23 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int i=1;
+        while (i<=n) {
+            int mid=i+(n-i)/2;
+            int res= guess(mid);
+            if (res==1) i=mid+1;
+            else if (res == -1) n=mid-1;
+            else if (res == 0) return mid;
+        }
+        return 1;
+    }
+};
